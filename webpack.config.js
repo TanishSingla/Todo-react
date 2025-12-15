f3c6e0b1dbaa6f065d3e6ddf4f76e1796a792cff
Tanish <tanish.singla@anywhere.co>
2025-12-15 18:06:23 +0530

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+import path from "path";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export default {
+  entry: "./src/main.js",
+
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+    ],
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+  ],
+
+  devServer: {
+    static: path.join(__dirname, "dist"),
+    port: 3001,
+  },
+
+  mode: "development",
+};
